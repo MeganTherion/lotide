aa461fe386952830de95390b8077b51e9e7bdc93
MeganTherion <meganhutt@hotmail.com>
2022-03-09 15:50:36 +0000

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+function eqArrays (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+//assertEqual(eqArrays([1, 2, 1,], [1, 2, 3]), true); 
+
+function assertArraysEqual (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`👍 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`👎 No good: ${arr1} !== ${arr2}`);
+
+  }
+}
+assertArraysEqual([1, 2, 3], [1, 2, "3"])
+assertArraysEqual([1, 2, 3], [3, 2, 1])
--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
 function eqArrays (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false
@@ -10,17 +19,6 @@ function eqArrays (arr1, arr2) {
   }
   return true
 }
-
-function assertArraysEqual (arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`)
-  }
-}
-
-
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 2, 3], [3, 2, 1])
-assertArraysEqual(['1', '2', '3'], ['1', '2', '3'])
-assertArraysEqual(['1', '2', '3'], ['1', '2', 3])
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays([1, 2, 1,], [1, 2, 3]), true); 
