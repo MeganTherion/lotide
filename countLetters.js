ba63d0392a916ecee2ff5f1ed34ebf76f7683fe4
MeganTherion <meganhutt@hotmail.com>
2022-03-10 06:28:46 +0000

    added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countLetters = function(sentence) {
+const letterCount = {};
+
+for (const letter of sentence) {
+  //console.log(letter);
+if (letterCount[letter]) {
+  letterCount[letter] += 1;
+} else {
+  letterCount[letter] = 1;
+}
+}
+return letterCount;
+}
+
+
+const newSentence = "LIGHTHOUSE";
+console.log(countLetters(newSentence));
