3c1636353003c1257074c160866c31e12649761c
MeganTherion <meganhutt@hotmail.com>
2022-03-15 04:15:49 +0000

    fixed middle again

--- middle.js
+++ middle.js
@@ -1,24 +1,11 @@
-//const assertEqual = require('./assertEqual');
-//const eqArrays = require('./eqArrays');
-//const assertArraysEqual = require('./assertArraysEqual');
-
-
 const middle = function(array) {
-  console.log("array", array);
   let middleInteger = [];
-  if (array.length % 2 === 0) {
-    middleInteger[0] = array[(array.length/2 - 1)];
-    console.log("middle int0", middleInteger[0]);
-    middleInteger[1] = array[array.length/2];
-    console.log("middle int1", middleInteger[0]);
-  } //else {
-    middleInteger = array[array.length/2]
-    console.log(middleInteger);
-  //}
-  console.log(middleInteger);
-  return (middleInteger);
-
-
-}
+  //if array is even 
+  if (array % 2 === 0); {
+  middleInteger = array[Math.floor((array.length -1)/2)];
+  } 
+  middleInteger = array[Math.floor(array.length / 2)];
+return middleInteger;
+  }
+  console.log(middle([1, 2, 3, 4, 5]));
 console.log(middle([1, 2, 3, 4, 5]));
-module.exports = middle;
