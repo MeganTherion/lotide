6864f56ce215c2aa5ce4b347e1b25812c9c42d24
MeganTherion <meganhutt@hotmail.com>
2022-04-13 23:59:45 +0000

    refactored eqArrays to include recursion

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,27 @@
 const assertEqual = require('./assertEqual')
 
 const eqArrays = function(a, b) {
-  console.log("a", a, "b", b);
+  //console.log("a", a, "b", b);
   if (a.length !== b.length) {
     return false;
   }
   for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
+    if (Array.isArray(a[i]) && Array.isArray(b[i])) {
+      console.log("arrays here");
+      if (!eqArrays(a[i], b[i])) {
+        return false;
+      }
+    }
+    else if (a[i] !== b[i]) {
       return false;
     }
   }
   return true;
 };
-  
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
+// console.log(eqArrays([1, 2], [1, 2]))
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+// eqArrays([[2, 3], [4]], [[2, 3], 4]) // => false 
 module.exports = eqArrays; 
  
 
