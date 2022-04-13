f422f2953d51dc5f529550ce82bd3e6a5c16e11b
MeganTherion <meganhutt@hotmail.com>
2022-04-13 23:20:36 +0000

    added recursive function to eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -12,6 +12,11 @@ const eqObjects = function(object1, object2) {
   }
   for (const key of keys1) { //for (const key of object1)
       if (keys1[key] === keys2[key]) {
+        //handle the case where both values are objects:
+      if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+        //console.log("aint arrays");
+        eqObjects(object1[key], object2[key]);
+      } 
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
        if (!eqArrays(object1[key], object2[key])) {
           return false;
@@ -20,8 +25,15 @@ const eqObjects = function(object1, object2) {
      return false;
        }
       }
-  }
+      
+      }
+  
   return true;
 };
+console.log(assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })), true); // => true
 
-module.exporsts = eqObjects;
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })), false); // => false
+console.log(assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })), false); // => false
+
+
+module.exports = eqObjects;
