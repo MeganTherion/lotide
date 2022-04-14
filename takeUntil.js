17d0d0ad522478eeb9b554a3a151f4554e9521fc
MeganAlexandroid <98662022+MeganTherion@users.noreply.github.com>
2022-04-14 16:36:59 -0700

    Update takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,14 +1,9 @@
 const takeUntil = function(array, callback) {
   const result = [];
-  for (let items of array) { // 1: loop through array
-    if (callback(items)) {  // if function x(items)
-      
+  for (let items of array) {
+    if (callback(items)) {
       return result;
-    } 
-    result.push(items)} //2. push items to array
-}
-
-module.exports = takeUntil;
-
-// let funk = x => x < 0 //name the function to test it
-// console.log(funk(data1[5])); //function checks to see if data1 is <0
+    }
+    result.push(items);
+  }
+};
