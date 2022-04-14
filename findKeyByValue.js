021b8cfd9e90ffbd1b968cf5f8b7f5ada6dd1d09
MeganAlexandroid <98662022+MeganTherion@users.noreply.github.com>
2022-04-14 16:29:37 -0700

    Update findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,18 +1,11 @@
-const assertEqual = require('./assertEqual');
 
-
-const countOnly = function(allItems, itemsToCount) {
-  const results = {}; //object that can hold stats: (count[name])
-  for (const item of allItems) { //inside the loop,
-    if (itemsToCount[item]) {
-      if (results[item]) { //increment a counter for each item:
-        results[item] += 1; //set a property with that string key to:
-      } else {              //the value that was already there
-        results[item] = 1;
-      }
+const findKeyByValue = function(object, value) {
+  for (let key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
     }
   }
-  return results;
+  return undefined;
 };
 
-module.exports = countOnly;
+module.exports = findKeyByValue;
