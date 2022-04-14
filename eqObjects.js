355647fa734cbe95e52df42eb73b63a2c4466ad4
MeganAlexandroid <98662022+MeganTherion@users.noreply.github.com>
2022-04-14 16:24:28 -0700

    Update eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -12,6 +12,9 @@ const eqObjects = function(object1, object2) {
   }
   for (const key of keys1) { //for (const key of object1)
       if (keys1[key] === keys2[key]) {
+        if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+          eqObjects(object1[key], object2[key]);
+        }
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
        if (!eqArrays(object1[key], object2[key])) {
           return false;
