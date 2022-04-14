293d5cdf0fe39e48080d2d54f5e53c5589585eac
MeganTherion <meganhutt@hotmail.com>
2022-04-14 23:03:07 +0000

    updated eqArrays.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,12 @@
-const assertEqual = require('./assertEqual');
+//const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
 
-function assertArraysEqual (arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === false) {
     return false;
-  } 
-    return true;
   }
-console.log(assertArraysEqual([1,2,3],[1,2,3]));
+  return true;
+};
 
 module.exports = assertArraysEqual;
