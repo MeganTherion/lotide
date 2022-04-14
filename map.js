d5b77620d8d225abee72321479bd36684225a1a0
MeganAlexandroid <98662022+MeganTherion@users.noreply.github.com>
2022-04-14 16:32:43 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,18 +1,14 @@
 const assertArraysEqual = require('./assertArraysEqual');
 const eqArrays = require('./eqArrays');
 
-
-//defining map
 const map = function(array, callback) {
+  
   const results = [];
   for (let item of array) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
-
-// const results1 = map(words, word => word[0]);
-// console.log(results1);
 
 module.exports = map;
