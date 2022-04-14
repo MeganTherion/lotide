1d6616d30d963767ad19576c8dcfec5f62440a4c
MeganAlexandroid <98662022+MeganTherion@users.noreply.github.com>
2022-04-14 16:33:29 -0700

    Update middle.js

--- middle.js
+++ middle.js
@@ -2,7 +2,6 @@ const assertEqual = require('./assertEqual');
 
 const middle = function(array) {
   let middleInteger = [];
-  //if array is even 
   if (array % 2 === 0); {
   middleInteger = array[Math.floor((array.length -1)/2)];
   } 
