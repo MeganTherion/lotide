54efae3ba31d9bc6f28088a8cae2cbdcd2ac5158
MeganAlexandroid <98662022+MeganTherion@users.noreply.github.com>
2022-04-16 18:53:31 -0700

    Updated tail.js with corrections

--- tail.js
+++ tail.js
@@ -1,6 +1,11 @@
 const assertEqual = require('./assertEqual');
-const tail = function(array) {
-  return array.slice(1);
-};
 
-module.exports = tail;
+const tail = function(array) {
+  let n = [];
+  if (array.length > 1) {
+    n.push(array.slice(1));
+  }
+  return n;
+};
+console.log(tail([1]));
+//module.exports = tail;
